7f831d117bcb15b88e433992fc69ceae537e459c
eenblam <eenblam@riseup.net>
2023-04-14 15:44:24 -0700

    #0: Refactor testing

--- 0/main_test.go
+++ 0/main_test.go
@@ -2,13 +2,17 @@ package main
 
 import (
 	"io"
-	"log"
 	"net"
-	"sync"
+	"testing"
+	"time"
 )
 
-func main() {
-	//addr := "127.0.0.1:9999"
+func TestMain(t *testing.T) {
+	go main()
+	// Let main warm up
+	time.Sleep(50 * time.Millisecond)
+
+	t.Parallel()
 	addr := net.TCPAddr{
 		net.ParseIP("127.0.0.1"),
 		9999,
@@ -29,15 +33,14 @@ func main() {
 		"**************************** (((((((((((((((((((((((((((((((((((((",
 	}
 
-	var wg sync.WaitGroup
-	for i, t := range tests {
-		wg.Add(1)
-		go func(i int, t string) {
-			defer wg.Done()
-			//fmt.Printf("Length: %d Test: %s\n", len(t), t)
+	for i, testCase := range tests {
+		// Weird scope footgun
+		i, testCase := i, testCase
+		t.Run(testCase, func(t *testing.T) {
+			t.Parallel()
 			conn, err := net.DialTCP("tcp", nil, &addr)
 			if err != nil {
-				log.Printf("TEST %d: couldn't dial TCP: %s\n", i, err)
+				t.Fatalf("TEST %d: couldn't dial TCP: %s\n", i, err)
 				return
 			}
 			defer conn.Close()
@@ -46,15 +49,15 @@ func main() {
 			times := 10000
 			written := 0
 			for j := 0; j < times; j++ {
-				w, err := conn.Write([]byte(t))
+				w, err := conn.Write([]byte(testCase))
 				if err != nil {
-					log.Printf("TEST %d: error writing to conn: %s\n", i, err)
+					t.Fatalf("TEST %d: error writing to conn: %s\n", i, err)
 				}
 				written += w
 			}
 			err = conn.CloseWrite()
 			if err != nil {
-				log.Printf("TEST %d: failed to close write: %s\n", i, err)
+				t.Fatalf("TEST %d: failed to close write: %s\n", i, err)
 			}
 
 			// Read and compare one byte at a time
@@ -65,26 +68,24 @@ func main() {
 				//TODO Test that conn is closed with brief timeout
 				if err != nil {
 					if err == io.EOF {
-						log.Printf("TEST %d: received EOF from server\n", i)
+						//log.Printf("TEST %d: received EOF from server", i)
 						conn.Close()
 						break
 					}
-					log.Printf("TEST %d: failed to read: %s\n", i, err)
+					t.Fatalf("TEST %d: failed to read: %s\n", i, err)
 				}
 				read += r
 				got := buf[0]
-				exp_index := (read - 1) % len(t)
-				expected := t[exp_index]
+				exp_index := (read - 1) % len(testCase)
+				expected := testCase[exp_index]
 				if got != expected {
-					log.Printf("TEST %d: got %b expected %b at byte %d\n", i, got, expected, read)
-					break
+					t.Fatalf("TEST %d: got %b expected %b at byte %d\n", i, got, expected, read)
 				}
 			}
 			// Test total is correct
-			if read != len(t)*times {
-				log.Printf("TEST %d: expected %d bytes, got %d\n", i, len(t)*times, read)
+			if read != len(testCase)*times {
+				t.Fatalf("TEST %d: expected %d bytes, got %d\n", i, len(testCase)*times, read)
 			}
-		}(i, t)
+		})
 	}
-	wg.Wait()
 }
