5cda3ad30f65ddbf823057544ba505a1a4a6c1ac
eenblam <eenblam@riseup.net>
2023-06-07 08:53:06 -0700

    Consistently quote variables in 9/deploy.sh

--- 9/deploy.sh
+++ 9/deploy.sh
@@ -7,14 +7,14 @@ KEY=~/.ssh/id_rsa_do
 # Just use existing droplet if it wasn't cleaned up somehow
 # (presumably by exiting too early)
 #		--size s-1vcpu-1gb \
-DROPLET=$(doctl compute droplet get $NAME --format=ID --no-header 2>/dev/null)
+DROPLET=$(doctl compute droplet get "$NAME" --format=ID --no-header 2>/dev/null)
 if [ -z "$DROPLET" ]; then
 	if ! DROPLET=$(doctl compute droplet create \
 		--region lon1 \
 		--image debian-11-x64 \
 		--size s-1vcpu-2gb \
 		--ssh-keys c8:79:0b:65:47:36:b8:77:83:8e:97:cf:c5:3b:90:0b \
-		$NAME \
+		"$NAME" \
 		--format=ID \
 		--no-header); then
 		echo "Couldn't create droplet ${NAME}. Exiting"
@@ -29,16 +29,16 @@ fi
 # Registering here, since we don't want to catch Ctrl+C prior.
 handle_interrupt() {
 	echo "Tearing down droplet ${NAME} with ID ${DROPLET}"
-	doctl compute droplet delete -f $DROPLET
+	doctl compute droplet delete -f "$DROPLET"
 }
 trap handle_interrupt SIGINT
 
 
 echo "Waiting for droplet IP"
-IP=$(doctl compute droplet get $DROPLET --format='PublicIPv4' --no-header 2>/dev/null)
+IP=$(doctl compute droplet get "$DROPLET" --format='PublicIPv4' --no-header 2>/dev/null)
 while [ -z "$IP" ]; do
 	sleep 5
-	IP=$(doctl compute droplet get $DROPLET --format='PublicIPv4' --no-header)
+	IP=$(doctl compute droplet get "$DROPLET" --format='PublicIPv4' --no-header)
 done
 
 echo "IP: ${IP}"
