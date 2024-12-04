dd4e1d63e32cf8b302242d3756ef6cc6faee0f9d
eenblam <eenblam@riseup.net>
2024-12-03 19:22:44 -0800

    7: improve application layer in main.go
    * Ensure session closed in handler
    * Scanner buffer should accommodate max packet size
    * Scanner requires different split function to avoid eating \r (CR)

--- 7/main.go
+++ 7/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"log"
 	"net"
 	"slices"
@@ -34,19 +35,49 @@ func main() {
 // reverseSessionHandler implements the application layer by simply reading until a new line
 // and then responding with a reversed copy of each line.
 func reverseSessionHandler(session *Session) {
+	defer session.Close()
+
 	scanner := bufio.NewScanner(session)
+	// Default token size is 64k; we might receive maxInt bytes before newline
+	buf := make([]byte, maxInt)
+	scanner.Buffer(buf, maxInt)
+	scanner.Split(ScanLinesNoCR)
+
 	for scanner.Scan() {
+		log.Println("SCAN")
 		data := scanner.Bytes()
-		log.Printf(`Reverse: Session[%s] received message: [%s]`, session.Key(), data)
+		log.Printf(`Reverse: Session[%s] received [%d] bytes`, session.Key(), len(data))
 		slices.Reverse(data)
 		data = append(data, '\n')
 		_, err := session.Write(data)
 		if err != nil {
 			log.Printf(`Reverse: Session[%s] encountered error on write: %s`, session.Key(), err)
+			session.sendClose()
+			break
 		}
-		log.Printf(`Reverse: Session[%s] sent message: [%s]`, session.Key(), data)
+		log.Printf(`Reverse: Session[%s] sent [%d] bytes`, session.Key(), len(data))
 	}
 	if err := scanner.Err(); err != nil {
 		log.Printf(`Reverse: Session[%s] scanner exited with error: %s`, session.Key(), err)
 	}
+}
+
+// ScanLinesNoCR works like bufio.ScanLines, but it doesn't try to strip carriage return (\r 0x0D).
+// This can cause several issues:
+// * Simply returning the wrong data when a \r is skipped
+// * Hanging on `abcd\r\nabcd\n` since it ignores the second \n in favor of an \r\n
+func ScanLinesNoCR(data []byte, atEOF bool) (advance int, token []byte, err error) {
+	if atEOF && len(data) == 0 {
+		return 0, nil, nil
+	}
+	if i := bytes.IndexByte(data, '\n'); i >= 0 {
+		// We have a full newline-terminated line.
+		return i + 1, data[0:i], nil
+	}
+	// If we're at EOF, we have a final, non-terminated line. Return it.
+	if atEOF {
+		return len(data), data, nil
+	}
+	// Request more data.
+	return 0, nil, nil
 }
